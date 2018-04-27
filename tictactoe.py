3449127760bc1ae59ced0a9bef4ad5a378e355b6
diegoballest <diego.alfredo96@gmail.com>
2018-04-26 21:29:15 -0500

    minimax

--- tictactoe.py
+++ tictactoe.py
@@ -36,7 +36,6 @@ def win(board, player):
         (board[0] == player and board[4] == player and board[8] == player) or
         (board[3] == player and board[4] == player and board[6] == player)
     ):
-        print("win")
         return True
     else:
         return False
@@ -48,19 +47,53 @@ def getEmptySpaces(board):
             emptySpaces.append(i)
     return emptySpaces
 
-
-def main():
-    board, boardArr = parse_string_to_game_format('e e e x o x o o e      ')
-
+def minimax(boardArr, player, score, depth):
     emptySpaces = getEmptySpaces(boardArr)
-    print(board)
-    print(boardArr)
-    print(emptySpaces)
+    if(not emptySpaces):
+        print("Final score",score)
     for i in emptySpaces:
         temp = copy.deepcopy(boardArr)
-        temp[i] = 'x'
-        print(temp)
-        win(temp, 'x');
+        temp[i] = player
+        tempString = ' '.join(temp)
+        tempBoard, tempBoardArr = parse_string_to_game_format(tempString)
+        print("-----------------------------------")
+        print(tempBoard)
+        print(tempBoardArr)
+        print(depth)
+        print("PATH", i)
+        if(win(temp, 'x')):
+            print("winX")
+            print("Score",score)
+            return 10
+        elif(win(temp,'o')):
+            print("winO")
+            print("Score",score)
+            return -10
+        else:
+            if(player == 'x'):
+
+                score += minimax(tempBoardArr, 'o', score, depth +1)
+                print("Score",score)
+            else:
+                score += minimax(tempBoardArr, 'x', score, depth +1)
+                print("Score",score)
+        print("-----------------------------------")
+    return 0
+
+
+
+
+def main():
+    initial = 'o o x x e o e e x'
+    print(initial)
+    board, boardArr = parse_string_to_game_format(initial)
+    print(board)
+    print(boardArr)
+    print()
+    minimax(boardArr, 'x', 0, 0)
+
+
+
 
 
 
