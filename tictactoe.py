1bf06136774abfd78cb3f132dfae06794fc0fc4a
Moises Montano <a01271656@itesm.mx>
2018-04-27 18:18:39 -0500

    finished minimax implementation

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,12 @@
 import copy
 
 '''
-this method receives an string
+this method receives a string
 and formats it if its a valid board.
 
 filter method is for removing whitespaces in array
 '''
+
 def parse_string_to_game_format(string):
     board = ''
     boardArr = string.split(' ')
@@ -15,13 +16,12 @@ def parse_string_to_game_format(string):
         return board
     for i, l, in enumerate(boardArr):
         if l == 'e':
-            l = ' '
+          l = ' '
         board += str(l)
         if(i+1) %3 != 0:
-            board += ' |'
+          board += ' |'
         if (i+1) %3 == 0:
-            board += '\n'
-
+          board += '\n'
     return board, boardArr
 
 #get the board current state and the player X or O
@@ -34,7 +34,7 @@ def win(board, player):
         (board[1] == player and board[4] == player and board[7] == player) or
         (board[2] == player and board[5] == player and board[8] == player) or
         (board[0] == player and board[4] == player and board[8] == player) or
-        (board[3] == player and board[4] == player and board[6] == player)
+        (board[2] == player and board[4] == player and board[6] == player)
     ):
         return True
     else:
@@ -47,52 +47,93 @@ def getEmptySpaces(board):
             emptySpaces.append(i)
     return emptySpaces
 
-def minimax(boardArr, player, score, depth):
+def minimax(boardArr, player, ai, human):
+    moves = [] # Hash array that looks like this -> [{'i': 1, 'score': 10}, {'i': 2, 'score': -10}]
     emptySpaces = getEmptySpaces(boardArr)
+
     if(not emptySpaces):
-        print("Final score",score)
+        return {'score' : 0}
+
     for i in emptySpaces:
         temp = copy.deepcopy(boardArr)
         temp[i] = player
+        new_move = {}
+        new_move['i'] = i
         tempString = ' '.join(temp)
         tempBoard, tempBoardArr = parse_string_to_game_format(tempString)
-        print("-----------------------------------")
-        print(tempBoard)
-        print(tempBoardArr)
-        print(depth)
-        print("PATH", i)
-        if(win(temp, 'x')):
-            print("winX")
-            print("Score",score)
-            return 10
-        elif(win(temp,'o')):
-            print("winO")
-            print("Score",score)
-            return -10
+
+        if(win(temp, ai)):
+            new_move['score'] = 10
+            return new_move
+        elif(win(temp, human)):
+            new_move['score'] = -10
+            return new_move
         else:
-            if(player == 'x'):
-
-                score += minimax(tempBoardArr, 'o', score, depth +1)
-                print("Score",score)
+            if(player == ai):
+                res = minimax(tempBoardArr, human, ai, human)
+                new_move['score'] = res['score']
             else:
-                score += minimax(tempBoardArr, 'x', score, depth +1)
-                print("Score",score)
-        print("-----------------------------------")
-    return 0
+                res = minimax(tempBoardArr, ai, ai, human)
+                new_move['score'] = res['score']
 
+        moves.append(new_move)
+		
+    best = {}
+    if(player == ai):
+      maxScore = -10000000
+      for move in moves:
+        if(move['score'] > maxScore):
+          best = move
+          maxScore = move['score']
+    else:
+      minScore = 10000000
+      for move in moves:
+        if(move['score'] < minScore):
+          best = move
+          minScore = move['score']
+    return best
 
+def endGame(board):
+	emptySpaces = getEmptySpaces(board)
+	if(not emptySpaces):
+		return "TIE"
 
+	if(win(board, 'x')):
+		return "WINNER: X"
+	elif(win(board, 'o')):
+		return "WINNER: O"
+
+	return -1
 
 def main():
-    initial = 'o o x x e o e e x'
-    print(initial)
-    board, boardArr = parse_string_to_game_format(initial)
-    print(board)
-    print(boardArr)
-    print()
-    minimax(boardArr, 'x', 0, 0)
+    ai = 'x'
+    human = 'o'
+
+    # if ai is first
+    boardStr = 'e e e e x e e e e'
+    turn = human
+
+    board, boardArr = parse_string_to_game_format(boardStr)
+    winner = endGame(boardArr)
+    while(winner == -1):
+	    board, boardArr = parse_string_to_game_format(boardStr)
+	    print(board)
+	    if(turn == human):
+	    	nextMove = int(input("\nType number >>"))
+	    	boardArr[nextMove] = turn
+	    	turn = ai
+	    else:
+	    	nextMove = minimax(boardArr, ai, ai, human)
+	    	nextMove = nextMove['i']
+	    	boardArr[nextMove] = turn
+	    	turn = human
+
+	    boardStr = ' '.join(boardArr)
+	    winner = endGame(boardArr)
 
 
+    board, boardArr = parse_string_to_game_format(boardStr)
+    print(board,"\n", winner)
 
 
 
